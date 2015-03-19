b18aee49a81ae786b234a3b614494cb0f4a96fa6
Meic <meic.williams@gmail.com>
2015-03-19 22:55:37 +0000

    initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from mysite.views import hello
+
+urlpatterns = patterns('',
+    url(r'^hello/$', hello),
+)
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^hello/$', hello),
+    url(r'^admin/', include(admin.site.urls)),
+)
